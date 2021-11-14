ee956104ad931985b7adbdd2e9fb723cd01c8261
Ashrakatahmed18 <AshrakatAhmed181999@gmail.com>
2021-11-14 18:38:10 +0200

    first commit

---
+++ TaskAshrakat.java
@@ -0,0 +1,9 @@
+public class TaskAshrakat {
+  public static void main(String[] args) {
+    System.out.println("The car is going as fast as it can!");
+  }
+
+  public void speed(int maxSpeed) {
+    System.out.println("Max speed is: " + maxSpeed);
+  }
+}
